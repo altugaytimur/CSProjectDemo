4b209e96d2308cee8c216a1c50e592d620d48f9e
Altug Aytimur <aytimuraltug@gmail.com>
2024-03-21 18:01:36 +0300

    Add project files.

---
+++ CSProjectDemo1/Abstracts/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProjectDemo1.Abstracts
+{
+    internal class Book
+    {
+    }
+}
